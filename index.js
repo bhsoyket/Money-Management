fdad14414aa14691adf9a7e83d5d7e75f7770a62
Belayet Hossain <2020belayethossain@gmail.com>
2020-01-25 23:42:08 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express   = require('express');
+const app       = express();
+const morgan    = require('morgan');
+const mongoose  = require('mongoose');
+const cors      = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req,res)=>{
+    res.json({message: 'Wellcome to my app'});
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{ console.log(`Server listen on port ${PORT}`) });
